ace9880da8d547f38ea34c89dc66fb594350f347
wc1229 <2825797934@qq.com>
2024-01-02 09:57:44 +0800

    learn devtool upgrade wc

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main (int argc, char **argv)
 {
     printf ("[main branch]Hello Yocto!\n");
-    printf ("Learn devtool upgrade\n");
+    printf ("wc Learn devtool upgrade\n");
 
     return 0;
 } /* ----- End of main() ----- */
